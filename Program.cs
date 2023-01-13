a62525adc1aaa77dbbd592d1eb3d6fccf022af8a
Irina <iirinau@mail.ru>
2023-01-14 00:59:11 +0500

    Added method of printing for arrays

--- Program.cs
+++ Program.cs
@@ -1,5 +1,27 @@
-﻿void FinalTask1 ()
+﻿FinalTask1();
+
+void FinalTask1 ()
 {
     string [] arrayInput ={"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
-    
+    PrintArrayByString(arrayInput);
+
+
+}
+
+
+
+// Метод вывода массива на экран в виде строки
+void PrintArrayByString(string [] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+        if (i == (array.Length - 1))
+        {
+           Console.WriteLine($"{array[i]}."); 
+        }
+        else
+        {
+           Console.Write($"{array[i]}, ");
+        }
+    }
 }
